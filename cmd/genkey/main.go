1522260bbe39d83b1473f79ad494bd458ab26026
liondandelion <anvyko.main@yandex.ru>
2025-07-30 09:49:57 +0300

    update genkey program

--- cmd/genkey/main.go
+++ cmd/genkey/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"crypto/rand"
+	"log"
 	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Specify filepath for the key")
+	}
 	key := make([]byte, 32)
 	rand.Read(key)
-	os.Stdout.Write(key)
+	err := os.WriteFile(os.Args[1], key, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
