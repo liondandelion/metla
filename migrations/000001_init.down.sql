c210b5c63ca729bf552799984e000e62b88de878
liondandelion <anvyko.main@yandex.ru>
2025-10-05 20:17:09 +0300

    add events table

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -2,3 +2,7 @@ drop table if exists users cascade;
 drop table if exists sessions cascade;
 drop index if exists sessions_expiry_idx cascade;
 drop table if exists otp cascade;
+drop type if exists event_link cascade;
+drop table if exists events cascade;
+drop function if exists events_gen_new_id cascade;
+drop trigger if exists events_trigger_gen_new_id on events cascade;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -20,3 +20,36 @@ create table if not exists otp
     otp bytea not null,
     foreign key (username) references users on delete cascade
 );
+
+create type event_link as
+(
+    id bigint,
+    author text
+);
+
+create table if not exists events
+(
+    id bigint,
+    author text references users on delete cascade,
+    title text not null,
+    description text not null,
+    date timestamp with time zone null,
+    links event_link[] null,
+    primary key (id, author)
+);
+
+create or replace function events_gen_new_id() returns trigger as
+$$
+begin
+    new.id := (
+        select coalesce(max(id), 0) + 1
+        from events
+        where author = new.author
+    );
+    return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger events_trigger_gen_new_id
+before insert on events
+for each row execute function events_gen_new_id();
